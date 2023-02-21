9dd51aab1671607fb9725472a369914335b453fd
Mansoor Khan <mansoorKh1098@gmail.com>
2023-02-22 01:14:47 +0530

    📝 Add: merge array, swap array

---
+++ Merge two sorted array/MergeTwoSortedArray.js
@@ -0,0 +1,8 @@
+//Q// Merge two sorted array?
+
+//Q// Merge two sorted array?
+
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [6, 7, 8, 9, 10];
+
+console.log([...arr1, ...arr2]);
---
+++ Swap two numbers without using a temp variable/SwapTwoNumber.js
@@ -0,0 +1,9 @@
+//Q// Swap two numbers without using a temp variable?
+
+const stringNum = [2, 3];
+
+let [first, second] = stringNum;
+
+[second, first] = [first, second];
+
+console.log(first, second);
