fcf16abb7b87f87545fab3aa092cac919aa2148e
Mansoor Khan <mansoorKh1098@gmail.com>
2023-02-22 11:59:56 +0530

    📝 Add: random number

---
+++ Generate random between 5 to 7 by using defined function/generatingRandomNumber.js
@@ -0,0 +1,6 @@
+//Q// Generate random between 5 to 7 by using defined function.//done
+
+function randomGenerator(value) {
+  return Math.floor(Math.random() * 3) + 5;
+}
+console.log(randomGenerator());
